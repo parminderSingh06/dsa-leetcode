54bb9017b6117b33a582989c46d6ab57266d1ca3
Parminder Singh <parmindersingh1086@gmail.com>
2025-07-05 21:43:50 -0400

    Added a return in loop to speed up runtime even more.

--- arrays/lc/TwoSum.java
+++ arrays/lc/TwoSum.java
@@ -5,20 +5,25 @@ import java.util.HashMap;
 public class TwoSum {
 
     public int[] bruteForce(int[] nums, int target){
-
+        int[] ans = new int[2];
+        //Implement brute force.
+        return ans;
     }
 
     public int[] hashMap(int[] nums, int target){
         HashMap<Integer,Integer> map = new HashMap<>();
         int[] ans = new int[2];
 
-        for(int i: nums){
-            if(map.containsKey(target - ans[i])){
+        for(int i=0; i<nums.length;i++){
+            int diff = target - nums[i];
+
+            if(map.containsKey(diff)){
                 ans[0] = i;
-                ans[1] = map.get(target-ans[i]);
+                ans[1] = map.get(diff);
+                return ans;
             }
             else{
-                map.put(target-ans[i], i);
+                map.put(nums[i], i);
             }
         }
         return ans;
