08c1ebdfa21abc42ee2a6b6faeb88888f2396ebb
Parminder Singh <parmindersingh1086@gmail.com>
2025-07-07 16:20:13 -0400

    Completed brute force for two sum.

--- arrays/lc/TwoSum.java
+++ arrays/lc/TwoSum.java
@@ -6,7 +6,15 @@ public class TwoSum {
 
     public int[] bruteForce(int[] nums, int target){
         int[] ans = new int[2];
-        //Implement brute force.
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length-1;j++){
+                if(nums[i] + nums[j] == target){
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;
+                }
+            }
+        }
         return ans;
     }
 
