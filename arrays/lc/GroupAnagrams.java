453ea998c33c4943af2d11453c54da78632b290c
Parminder Singh <parmindersingh1086@gmail.com>
2025-07-07 16:32:39 -0400

    Completed isAnagram, and currently working on groupAnagrams.

---
+++ arrays/lc/GroupAnagrams.java
@@ -0,0 +1,12 @@
+package arrays.lc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution{
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> answer = new ArrayList<>();
+
+        return answer;
+    } 
+}
---
+++ arrays/lc/ValidAnagram.java
@@ -0,0 +1,20 @@
+package arrays.lc;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) return false;
+
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+
+        Arrays.sort(charS);
+        Arrays.sort(charT);
+
+        return Arrays.equals(charS, charT);
+        
+    }
+}
