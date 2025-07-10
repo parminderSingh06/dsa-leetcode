1b8ea3218b7b0cc2b703efc433ab66971adbbdc7
Parminder Singh <parmindersingh1086@gmail.com>
2025-07-10 19:25:13 -0400

    Completed 3Sum ;) (notes)

--- two-pointer/ThreeSum.java
+++ two-pointer/ThreeSum.java
@@ -6,16 +6,19 @@ public class ThreeSum {
 
     public List<List<Integer>> threeSum(int[] nums) {
         
+        //Create the list of lists and sort the orignal array so we can preform the two pointer apporach on the problem.
         List<List<Integer>> answerList = new ArrayList<>();
         Arrays.sort(nums);
 
+        //A loop to run through the numbers one by one which will turn this problem into two pointer as we are locking down one of the numbers.
         for(int i=0;i<nums.length;i++){
 
+            //Skip past a number that we had already locked down because if we dont the same triplet will show up again, take example [-2 -2 0 1 2]. 
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            
+
             int left = i+1;
             int right = nums.length-1;
-
+            //Two pointer approach
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum > 0){
@@ -28,6 +31,7 @@ public class ThreeSum {
                     answerList.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
+                    //if teh same numbers show up for the left pointer we have to mave it because we risk the same triplet being added again, take example: [-1 -1 -1 1 1 2].
                     while(left < right && nums[left] == nums[left-1]){left++;}
 
                 }
