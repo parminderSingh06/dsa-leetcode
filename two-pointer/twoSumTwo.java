80fb3d2982a58fc24b42f002891fc24d8ee5b921
Parminder Singh <parmindersingh1086@gmail.com>
2025-07-10 17:50:36 -0400

    Fixed small error.

--- two-pointer/twoSumTwo.java
+++ two-pointer/twoSumTwo.java
@@ -1,15 +1,16 @@
 public class twoSumTwo {
     public int[] twoSum(int[] numbers, int target) {
-        int[] answer = new int[2];
 
+        //Created the answer 
+        int[] answer = new int[2];
         int pointerOne = 0;
-        int pointerTwo = numbers.length;
+        int pointerTwo = numbers.length-1;
 
         while(pointerOne < pointerTwo){
             int twoSum = numbers[pointerOne] + numbers[pointerTwo];
             if(twoSum == target){
-                answer[0] = pointerOne;
-                answer[1] = pointerTwo;
+                answer[0] = pointerOne+1;
+                answer[1] = pointerTwo+1;
                 return answer;
             }
             else{
