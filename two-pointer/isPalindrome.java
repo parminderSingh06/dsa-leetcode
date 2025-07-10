8ef4989c48a2a3db124e1feb93467306152eb7c6
Parminder Singh <parmindersingh1086@gmail.com>
2025-07-10 17:29:55 -0400

    Added comments on isPalidrome code.

--- two-pointer/isPalindrome.java
+++ two-pointer/isPalindrome.java
@@ -1,14 +1,18 @@
 public class isPalindrome {
     public boolean isPalindrome(String s) {
-        String lowerCase = s.toLowerCase();
 
+        //Processes string to convert into lowercase first and then use the replaceAll method to remove any none alphanumeric characters.
+        String lowerCase = s.toLowerCase();
         String finalString = lowerCase.replaceAll("[^a-z0-9]","");
 
+        //Make char array so we can check each character and compare it with other characters.
         char[] array = finalString.toCharArray();
 
+        //Creatng two pointer so we can reference start and end of array and track if the letters match.
         int pointerOne = 0;
         int pointerTwo = finalString.length()-1;
 
+        //While loop that runs until the the pointers cross each other which would mean that the string is valid palidronme since all the letters matched through and through.
         while(pointerOne <= pointerTwo){
             if(array[pointerOne] != array[pointerTwo]){
                 return false;
