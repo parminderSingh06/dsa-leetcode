9cb0545334b5b74f3fd7b38d3ff9e1891f597e41
Parminder Singh <parmindersingh1086@gmail.com>
2025-08-14 20:33:52 -0400

    Completed using naive implementation by using a stack.

---
+++ linked-list/ReverseLinkedList.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        if(head == null) return head;
+        ListNode current = head;
+        Stack<Integer> stack = new Stack<>();
+        while(current != null){
+            stack.push(current.val);
+            current = current.next;
+        }
+        ListNode newHead = new ListNode(stack.pop());
+        current = newHead;
+        while(!stack.empty()){
+            ListNode newNode = new ListNode(stack.pop());
+            current.next = newNode;
+            current = current.next;
+        }
+        
+        return newHead;
+
+    }
+}
