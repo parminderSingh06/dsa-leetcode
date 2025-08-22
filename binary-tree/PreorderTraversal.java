a70144a2e0e7a545cafb7fad15412a199ea29058
Parm <parmindersingh1086@gmail.com>
2025-08-22 18:42:51 -0400

    Completed iterative dfs version of preorder traversal

---
+++ binary-tree/PreorderTraversal.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class PreorderTraversal {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+   public List<Integer> preorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> answer = new ArrayList<>();
+
+        if(root == null) return answer;
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+            TreeNode current = stack.pop();
+            answer.add(current.val);
+            if(current.right != null) stack.push(current.right);
+            if(current.left != null) stack.push(current.left);
+
+        }
+
+        return answer;
+    }
+}
