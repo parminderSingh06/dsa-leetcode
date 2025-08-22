6d5953061f74ab544a939b58cc8888930e0284ad
Parm <parmindersingh1086@gmail.com>
2025-08-22 18:55:19 -0400

    Completed recursive version of dfs for preorder traversal

--- binary-tree/PreorderTraversal.java
+++ binary-tree/PreorderTraversal.java
@@ -18,20 +18,30 @@ public class PreorderTraversal {
     }
 
    public List<Integer> preorderTraversal(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
+        // Stack<TreeNode> stack = new Stack<>();
         List<Integer> answer = new ArrayList<>();
 
-        if(root == null) return answer;
-        stack.push(root);
+        // if(root == null) return answer;
+        // stack.push(root);
         
-        while(!stack.isEmpty()){
-            TreeNode current = stack.pop();
-            answer.add(current.val);
-            if(current.right != null) stack.push(current.right);
-            if(current.left != null) stack.push(current.left);
+        // while(!stack.isEmpty()){
+        //     TreeNode current = stack.pop();
+        //     answer.add(current.val);
+        //     if(current.right != null) stack.push(current.right);
+        //     if(current.left != null) stack.push(current.left);
 
-        }
+        // }
+
+        recursion(answer, root);
 
         return answer;
+    }
+
+    public void recursion(List<Integer> list, TreeNode root){
+        if(root == null) return;
+
+        list.add(root.val);
+        recursion(list, root.left);
+        recursion(list, root.right);
     }
 }
