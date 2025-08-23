bcdb6929c9eeb99fc69451ba716eeef44bcc98c4
Parm <parmindersingh1086@gmail.com>
2025-08-23 16:37:13 -0400

    Completed recursive solution using dfs for invert binary tree

---
+++ binary-tree/InvertBinaryTree.java
@@ -0,0 +1,30 @@
+public class InvertBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+
+    public void dfs(TreeNode root){
+        if (root == null) return;
+        
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+        dfs(root.left);
+        dfs(root.right);
+    }
+}
