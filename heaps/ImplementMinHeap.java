a0b04b9a0f68558c5916964f770e6440335e819b
Parm <parmindersingh1086@gmail.com>
2025-08-29 21:06:56 -0400

    Completed insert and heapify up for heap impl

---
+++ heaps/ImplementMinHeap.java
@@ -0,0 +1,29 @@
+package heaps;
+
+import java.util.ArrayList;
+
+public class ImplementMinHeap {
+    
+    ArrayList<Integer> heap = new ArrayList<>();
+
+    public void insert(int value){
+        heap.add(heap.size(),value);
+        heapifyUp(heap.size()-1);
+    }
+
+    public void heapifyUp(int i){
+        if(i == 0) return;
+        int parentIndex = (i-1)/2;
+        while(i > 0 && heap.get(parentIndex) > heap.get(i)){
+            int temp = heap.get(parentIndex);
+            heap.set(parentIndex, heap.get(i));
+            heap.set(i, temp);
+            i = parentIndex;
+            parentIndex = (i-1)/2;
+        }
+    }
+
+    public void heapifyDown(int i){
+        
+    }
+}
