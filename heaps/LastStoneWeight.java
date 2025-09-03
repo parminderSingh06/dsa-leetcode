621ef233973afbb75abbf07ff75997591623b57c
Parminder Singh <parmindersingh1086@gmail.com>
2025-09-03 16:56:42 -0400

    Completed last stone weight

---
+++ heaps/LastStoneWeight.java
@@ -0,0 +1,25 @@
+package heaps;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class LastStoneWeight {
+
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int stone : stones){
+            maxHeap.add(stone);
+        }
+
+        while(maxHeap.size() > 1){
+            int y = maxHeap.poll();
+            int x = maxHeap.poll();
+            if(y - x != 0) maxHeap.add(y-x);
+        }
+
+        if(maxHeap.size() == 1) return maxHeap.peek();
+        else return 0;
+    }    
+
+}
