2dfda8e412c145700fac007f933ce652edd0ced7
Parminder Singh <parmindersingh1086@gmail.com>
2025-09-08 15:52:48 -0400

    Completed majority element using sorting

--- arrays/lc/MajorityElement.java
+++ arrays/lc/MajorityElement.java
@@ -1,20 +1,10 @@
 package arrays.lc;
 
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class MajorityElement {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        map.put(nums[0], 1);
-        int mostCommon = nums[0];
-
-        for(int i=1;i<nums.length;i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-            if(map.get(nums[i]) > map.get(mostCommon)){
-                mostCommon = nums[i];
-            }
-        }
-
-        return mostCommon;
+        Arrays.sort(nums);
+        return nums[nums.length/2];
     }   
 }
