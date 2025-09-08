1b7250d1019b7ee914693332c645d416c6b80f32
Parminder Singh <parmindersingh1086@gmail.com>
2025-09-08 15:23:51 -0400

    Completed remove element

---
+++ arrays/lc/RemoveElement.java
@@ -0,0 +1,23 @@
+package arrays.lc;
+
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        
+        int currentSize = nums.length;
+
+        int count = 0;
+
+        while(count < currentSize){
+            if(nums[count] == val){
+                nums[count] = nums[currentSize-1];
+                currentSize--;
+                continue;
+            }
+            else{
+                count++;
+            }
+        }
+
+        return currentSize;
+    }    
+}
