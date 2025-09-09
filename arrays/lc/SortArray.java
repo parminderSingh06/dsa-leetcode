0c570ca9002816597ab993ade9f0246e7b83b66b
Parm <parmindersingh1086@gmail.com>
2025-09-09 18:17:48 -0400

    Completed using sort array using merge \sort.

--- arrays/lc/SortArray.java
+++ arrays/lc/SortArray.java
@@ -1,5 +1,7 @@
 package arrays.lc;
 
+import java.util.Arrays;
+
 public class SortArray {
 
     public int[] InsertionSortArray(int[] nums) {
@@ -15,5 +17,50 @@ public class SortArray {
             }
         }
         return nums;
+    }
+
+    public int[] sortArray(int[] nums){
+        if(nums.length==1) return nums;
+        
+        int mid = nums.length/2;
+
+        int[] left = Arrays.copyOfRange(nums, 0, mid);
+        int[] right = Arrays.copyOfRange(nums, mid, nums.length);
+
+        return merge(sortArray(left), sortArray(right));
+    }
+
+    public int[] merge(int[] left, int[] right){
+        int[] result = new int[left.length+right.length];
+
+        int i=0;
+        int j=0;
+        int k=0;
+
+        while(i < left.length && j < right.length){
+            if(left[i] < right[j]){
+                result[k] = left[i];
+                i++;
+                k++;
+            }
+            else{
+                result[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+
+        while(i<left.length){
+            result[k] = left[i];
+            i++;
+            k++; 
+        }
+        while(j<right.length){
+            result[k] = right[j];
+            j++;
+            k++;
+        }
+
+        return result;
     }
 }
