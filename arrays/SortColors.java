63d9d6f0a99d46375ccfccdf49f9043477de27b5
Parm <parmindersingh1086@gmail.com>
2025-09-11 15:06:58 -0400

    Completed sort colors using pointer approach

--- arrays/SortColors.java
+++ arrays/SortColors.java
@@ -1,19 +1,31 @@
 package arrays;
 
-import java.util.PriorityQueue;
-
 class SortColors {
-        public void sortColors(int[] nums) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-
-        for(int num : nums){
-            heap.add(num);
-        }
+    public void sortColors(int[] nums) {
+        int right = nums.length-1;
+        int left = 0;
         int i = 0;
-        while(!heap.isEmpty()){
-            nums[i] = heap.poll();
+
+        while(i <= right){
+            if(nums[i] == 0){
+                swap(left, i, nums);
+                left++;
+            }
+            else if(nums[i] == 2){
+                swap(right, i, nums);
+                right--;
+                i--;
+            }
             i++;
         }
+
+        
+    }
+
+    public void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     
 }
