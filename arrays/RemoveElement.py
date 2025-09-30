fdee2598800337e9449a924664740ca8f7dd8153
Parminder Singh <parmindersingh1086@gmail.com>
2025-09-29 20:18:54 -0400

    Completed remove element in python

---
+++ arrays/RemoveElement.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        length = len(nums)
+        p = 0
+
+        while p < length:
+            if nums[p] == val:
+                nums[p] = nums[length-1]
+                length = length-1
+            else:
+                p = p+1
+        
+        return length
+
+        
