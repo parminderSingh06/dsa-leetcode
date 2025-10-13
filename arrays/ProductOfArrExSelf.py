aae6c05004e037962483e1ff1b08ea963c569081
Parminder Singh <parmindersingh1086@gmail.com>
2025-10-13 19:56:10 -0400

    solved prod of arr using no extra space

--- arrays/ProductOfArrExSelf.py
+++ arrays/ProductOfArrExSelf.py
@@ -3,21 +3,18 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        output = []
-        prefix = []
-        postfix = [None] * len(nums)
+        output = []   
+        prefix = 1
+
+        for i in range(len(nums)):
+            output.append(prefix)
+            prefix = nums[i] * prefix
+
+        p = len(nums)-1
+        postfix = 1
         
-        for i in range(len(nums)):
-            if i==0: prefix.append(nums[i]*1)
-            else: prefix.append(nums[i]*prefix[i-1])
-        print(prefix)
         for i in range(len(nums)-1,-1,-1):
-            if i==len(nums)-1: postfix[i] = nums[i]*1
-            else: postfix[i] = nums[i] * postfix[i+1]
-        print(postfix)
-        for i in range(len(nums)):
-            if i==len(nums)-1: output.append(prefix[i-1] * 1)
-            elif i==0: output.append(postfix[i+1] * 1)
-            else: output.append(prefix[i-1] * postfix[i+1])
+            output[i] = postfix * output[i]
+            postfix = nums[i] * postfix
 
         return output
