e4c5608b370cf580347af950c46765a646566510
Parminder Singh <parmindersingh1086@gmail.com>
2025-10-20 20:21:39 -0400

    Solved in O(n) running time using hashset

--- arrays/LongestConsecutiveSequence.py
+++ arrays/LongestConsecutiveSequence.py
@@ -4,18 +4,16 @@ from typing import List
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if len(nums) == 0: return 0
-        nums = sorted(nums)
-        longest_sequence = 1
-        current_sequence = 1
-        print(nums)
-        for i in range(len(nums)-1):
-            if nums[i]+1 == nums[i+1]:
-                current_sequence += 1
-            elif nums[i] == nums[i+1]:
-                print(nums[i], nums[i+1])
-            else:
-                longest_sequence = max(current_sequence, longest_sequence)
-                current_sequence = 1    
-        longest_sequence = max(current_sequence, longest_sequence)  
-        return longest_sequence        
 
+        hset = set(nums)
+        longest = 1
+
+        for num in hset:
+            if num-1 not in hset:
+                curr_seq = 1
+                next_num = num + 1
+                while next_num in hset:
+                    curr_seq += 1
+                    next_num += 1
+                longest = max(curr_seq, longest)
+        return longest
