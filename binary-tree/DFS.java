c6d9c57b1408b16b221833d6a8eecd55eb64fac0
Parminder Singh <parmindersingh1086@gmail.com>
2025-10-30 22:12:48 -0400

    Completed dfs for memory sake.

---
+++ binary-tree/DFS.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class DFS {
+    
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    
+    public void dfs(TreeNode head){
+        Stack<TreeNode> stack = new Stack<>();
+        ArrayList<TreeNode> visited = new ArrayList<>();
+        TreeNode current = head;
+        stack.push(current);
+        while(!stack.isEmpty()){
+            current = stack.pop();
+            visited.add(current);
+            if(current.right != null) stack.push(current.right);
+            if(current.left != null) stack.push(current.left);
+        }
+    }
+}
