5d9af0071a43ad7f43cdf1de3b43d02483e3615d
Parminder Singh <parmindersingh1086@gmail.com>
2025-11-15 19:29:12 -0500

    Completed the problem using a array instead of hashmap to speed algorithim up.

--- sliding-window/LongestRepeatingCharacter.java
+++ sliding-window/LongestRepeatingCharacter.java
@@ -1,22 +1,21 @@
-import java.util.HashMap;
-
 public class LongestRepeatingCharacter {
 
     public int characterReplacement(String s, int k) {
         int answer = 0;
-        HashMap<Character,Integer> map = new HashMap<>();
+        int[] freq = new int[26];
         int left = 0, right = 0;
         int maxFrequency = 0;
 
         while(right<s.length()){
             char ch = s.charAt(right);
 
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
-            maxFrequency = Math.max(maxFrequency, map.get(ch));
+
+            freq[ch - 'A']++;
+            maxFrequency = Math.max(maxFrequency, freq[ch-'A']);
 
             while(((right-left+1) - maxFrequency) > k){
                 char leftCh = s.charAt(left);
-                map.put(leftCh, map.get(leftCh) - 1);
+                freq[leftCh-'A']--;
                 left++;
             }
             answer = Math.max(answer, (right-left+1));
