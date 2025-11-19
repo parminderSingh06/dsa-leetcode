db44e137a357c8e682f80b83107d7d26b46c69ee
Parminder Singh <parmindersingh1086@gmail.com>
2025-11-18 23:37:51 -0500

    Did not realize I did not need a map or while loop to set to zero as the array values intialize at zero

--- stack/DailyTemperatures.java
+++ stack/DailyTemperatures.java
@@ -1,35 +1,21 @@
 package stack;
-
-import java.util.HashMap;
 import java.util.Stack;
 
 public class DailyTemperatures {
     public int[] dailyTemperatures(int[] temperatures) {
-        HashMap<Integer,Integer> map = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
         int[] answer = new int[temperatures.length];
 
         for(int i=0;i<temperatures.length;i++){
             int todayTemp = temperatures[i];
-            map.put(i, todayTemp);
 
-            while(!stack.isEmpty() && map.get(stack.peek()) < todayTemp){
-                int prevDay = stack.peek();
-
+            while(!stack.isEmpty() && temperatures[stack.peek()] < todayTemp){
+                int prevDay = stack.pop();
                 answer[prevDay] = i - prevDay;
-
-                stack.pop();
             }
 
             stack.add(i);
-
         }
-
-        while(!stack.isEmpty()){
-            int temp = stack.pop();
-            answer[temp] = 0;
-        }
-
         return answer;
-    }    
+    }   
 }
